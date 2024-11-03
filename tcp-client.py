8a5540fd0e0236c8b679b518e6a06526c71bc12c
Gadeev Dmitry <gadeev.dima@mail.ru>
2024-11-03 20:49:52 +0300

    raw tcp server & client implemented

---
+++ tcp-client.py
@@ -0,0 +1,34 @@
+import socket
+
+def Print_message_from_server(sock):
+    message = sock.recv(100)
+    if not message:
+        return False
+
+    print(message)
+    return True
+
+Welcome_from_server = Print_message_from_server
+Response_on_attempt = Print_message_from_server
+
+def Client():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("server", 1000))
+
+    try:
+        while True:
+            if not Welcome_from_server(sock):
+                break
+
+            while True:
+                client_attempt = input()[: 100]
+                sock.sendall(client_attempt)
+
+                if not Response_on_attempt(sock):
+                    break
+            break
+    finally:
+        sock.close()
+
+if __name__ == "__main__":
+    Client()
---
+++ tcp-server.py
@@ -0,0 +1,70 @@
+import random
+import socket
+
+NUMBER_MIN = 1
+NUMBER_MAX = 100
+
+def Welcome_client(connect_sock):
+    welcome_message = "Welcome, TCP-client!\nGuess the number from 1 to 100."
+    connect_sock.sendall(welcome_message)
+
+def Handle_client_attempt(connect_sock, hidden_number, client_attempt):
+    response_message = ""
+    is_guessed       = False
+
+    while True:
+        if not client_attempt.isdigit():
+            response_message = "You should print the only number"
+            break
+        client_number = int(client_attempt)
+
+        if client_number < NUMBER_MIN or client_number > NUMBER_MAX:
+            response_message = "Your number is out of range"
+
+        elif client_number == hidden_number:
+            response_message = "God damn!\nGame is over! Let's do it again!"
+            is_guessed = True
+
+        elif client_number < hidden_number:
+            response_message = "Hidden number is larger"
+
+        else:
+            response_message = "Hidden number is lower"
+
+        break
+    connect_sock.sendall(response_message)
+
+    return is_guessed
+
+def Start_new_game():
+    return random.randint(NUMBER_MIN, NUMBER_MAX)
+
+def Server():
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock = socket.bind(("server", 1000))
+
+    server_sock.listen(1)
+    try:
+        while True:
+            connect_sock, connect_addr = server_sock.accept()
+            Welcome_client(connect_sock)
+
+            try:
+                hidden_number = Start_new_game()
+                while True:
+                    client_attempt = connect_sock.recv(100)
+                    if not client_attempt:
+                        break
+
+                    if Handle_client_attempt(connect_sock, hidden_number, client_attempt):
+                        hidden_number = Start_new_game()
+            finally:
+                print("connect_sock.close()")
+                connect_sock.close()
+                raise
+    finally:
+        print("server_sock.close()")
+        server_sock.close()
+
+if __name__ == "__main__":
+    Server()
